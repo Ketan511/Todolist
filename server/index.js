d3e1e248faabc69e43f7f2f7525c4d350614e576
Ketan <ketanmali511@gmail.com>
2022-11-04 19:30:23 +0530

    Adding to do list

---
+++ server/index.js
@@ -0,0 +1,96 @@
+const express =require("express");
+const app=express();
+const cors=require("cors");
+const pool=require("./db");
+
+//midelwares
+app.use(cors());
+app.use(express.json()); //req.body
+
+
+//ROUTES
+
+
+//Create To Do 
+app.post("/todos",async(req,res)=> 
+{
+    try {
+        // console.log(req.body);
+        const {description}=req.body;
+        const newTodo=await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING *",
+            [description]
+        );
+
+        res.json(newTodo.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+
+//Get all To dos
+
+app.get("/todos",async(req,res)=>
+{
+    try {
+        const allTodos=await pool.query
+        ("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+
+//Get To Do
+
+app.get("/todos/:id",async (req,res)=>
+{
+    try {
+        const {id}=req.params;
+        const todo=await pool.query("SELECT * FROM todo WHERE todo_id=$1",[id])
+
+        res.json(todo.rows[0])
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+//Update To do
+
+app.put("/todos/:id", async(req,res) =>
+{
+    try {
+        const {id}=req.params;
+        const {description}=req.body;
+        const updateTodo=await pool.query
+        ("UPDATE todo SET description = $1 WHERE todo_id= $2",
+        [description,id]);
+        res.json("Todo was updated!")
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+//Delete To do
+
+app.delete("/todos/:id", async(req,res)=>
+{
+    try {
+        const {id}=req.params;
+        const deletTodo=await pool.query
+        ("DELETE FROM todo WHERE todo_id=$1",[id]);
+        res.json("Todo was deleted");
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+app.listen(5000,()=> {
+    console.log("server is running on port 5000");
+});
